1c862cae0169818ac15576d8d26ddfc067c72248
Makopoppo <bokemako@gmail.com>
2013-06-17 19:11:43 +0900

    Fix for Sublime Text 3

--- init.py
+++ init.py
@@ -1,8 +1,9 @@
 import os
+import stat
 import sublime
 
 # add permissions to lslint executables
 binaryPath = os.path.join(sublime.packages_path(), "LSL/osx/lslint")
-os.chmod(binaryPath, 0755)
+os.chmod(binaryPath, stat.S_IEXEC)
 binaryPath = os.path.join(sublime.packages_path(), "LSL/linux/lslint")
-os.chmod(binaryPath, 0755)
+os.chmod(binaryPath, stat.S_IEXEC)
