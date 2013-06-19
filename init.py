bd11eff2270e1a30594f468d6a42d97b91ae75b5
Makopoppo <bokemako@gmail.com>
2013-06-19 22:38:52 +0900

    damn lslint should be 755, not 100

--- init.py
+++ init.py
@@ -4,6 +4,6 @@ import sublime
 
 # add permissions to lslint executables
 binaryPath = os.path.join(sublime.packages_path(), "LSL/osx/lslint")
-os.chmod(binaryPath, stat.S_IEXEC)
+os.chmod(binaryPath, stat.S_IRUSR|stat.S_IWUSR|stat.S_IXUSR|stat.S_IRGRP|stat.S_IXGRP|stat.S_IROTH|stat.S_IXOTH)
 binaryPath = os.path.join(sublime.packages_path(), "LSL/linux/lslint")
-os.chmod(binaryPath, stat.S_IEXEC)
+os.chmod(binaryPath, stat.S_IRUSR|stat.S_IWUSR|stat.S_IXUSR|stat.S_IRGRP|stat.S_IXGRP|stat.S_IROTH|stat.S_IXOTH)
